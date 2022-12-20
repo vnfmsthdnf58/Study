856a16c75041249ccedd5db9b067a05d17fe7dc1
KTS <thdnf586@naver.com>
2022-12-20 17:39:33 +0900

    push 테스트

--- 1-vanilla/js/Store.js
+++ 1-vanilla/js/Store.js
@@ -2,8 +2,9 @@ const tag = "[store]";
 
 export default class Store {
   constructor(storage) {
+    console.log(tag, 'store');
     if (!storage) throw "no storage";
-
+    
     this.storage = storage;
   }
 }
--- 1-vanilla/js/main.js
+++ 1-vanilla/js/main.js
@@ -1,15 +1,21 @@
 import Controller from "./Controller.js";
 import Store from "./store.js";
 import storage from "./storage.js";
+import SearchFormView from "./views/SearchFormVeiw.js";
 
-document.addEventListener("DOMContentLoaded", main);
+const tag = '[main]'
+document.addEventListener("DOMContentLoaded", main);  // 돔이 로딩되었을때(실행됬을때) main 함수를 실행하라 
 
 function main() {
-  const store = new Store(storage);
+  console.log(tag, 'main');
+  
+  const store = new Store(storage); // storage 모델을 생성한다.
 
   const views = {
-    // TODO
+    searchFormView: new SearchFormView()
   };
 
-  new Controller(store, views);
+  // store, views 생성자 함수를 만들어주는 구조
+
+  new Controller(store, views); // 생성자 함수를 넣어줘서 컨트롤러 객체를 만들어준다.
 }
